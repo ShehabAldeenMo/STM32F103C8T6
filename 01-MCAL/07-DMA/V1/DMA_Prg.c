ad3d214b0dbe0e08efc1bc3835aa8b22e850227d
ShehabAldeenMo <shihab.rifai02@eng-st.cu.edu.eg>
2023-09-11 20:36:11 +0300

    Commit V3

--- 01-MCAL/07-DMA/V1/DMA_Prg.c
+++ 01-MCAL/07-DMA/V1/DMA_Prg.c
@@ -24,8 +24,61 @@
 #include "DMA_Interface.h"
 #include "DMA_Private.h"
 
-/* DMA Initialization with default configurations */
+/**
+ * \section Brief
+ * This function configure DMA channel.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Configuration_DMA  \ Type struct  \ Range determine from Configuration_DMA \ Resolution inapplicable \ unit block of memory
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_Config ->  :
+ *
+ * @enduml
+ */
 void DMA_Config(const Configuration_DMA *Config_Ptr){
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *   switch (Which DMA channel is activited)
+	 *   case ( condition channel1 )
+	 *     #green:Set configurations of channel1;
+	 *   case ( condition channel2 )
+	 *     #green:Set configurations of channel2;
+	 *   case ( condition channel3 )
+	 *     #green:Set configurations of channel3;
+	 *   case ( condition channel4 )
+	 *     #green:Set configurations of channel4;
+	 *   case ( condition channel5 )
+	 *     #green:Set configurations of channel5;
+	 *   case ( condition channel6 )
+	 *     #green:Set configurations of channel6;
+	 *   case ( condition channel7 )
+	 *     #green:Set configurations of channel7;
+	 *   case ( default )
+	 *     #red:Det_ReportError(infinite loop);
+	 *     end
+	 * endswitch
+	 * end
+	 * @enduml
+	 */
 	switch (Config_Ptr->Copy_Channel) {
 	case DMA_CH1:
 		DMA_CH1_BASE->CCR = Config_Ptr->CircularMode | Config_Ptr->Priority
@@ -63,12 +116,65 @@ void DMA_Config(const Configuration_DMA *Config_Ptr){
 				| Config_Ptr->pIncrement | Config_Ptr->INT_Enable;
 		break;
 	default:
-		/* invalid parameter */
+		Det_ReportError(0x00, 0x00, 0x00);
 	}
 }
 
-/* DMA channel Enable*/
+/**
+ * \section Brief
+ * This function enables DMA channel.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Channel_DMA  \ Type enum  \ Range determine from Channel_DMA \ Resolution inapplicable \ unit bits
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_ChannelEnable ->  :
+ *
+ * @enduml
+ */
 void DMA_ChannelEnable(Channel_DMA Copy_Channel){
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *   switch (Which DMA channel is activited)
+	 *   case ( condition channel1 )
+	 *     #green:Turn on channel1;
+	 *   case ( condition channel2 )
+	 *     #green:Turn on channel2;
+	 *   case ( condition channel3 )
+	 *     #green:Turn on channel3;
+	 *   case ( condition channel4 )
+	 *     #green:Turn on channel4;
+	 *   case ( condition channel5 )
+	 *     #green:Turn on channel5;
+	 *   case ( condition channel6 )
+	 *     #green:Turn on channel6;
+	 *   case ( condition channel7 )
+	 *     #green:Turn on channel7;
+	 *   case ( default )
+	 *     #red:Det_ReportError(infinite loop);
+	 *     end
+	 * endswitch
+	 * end
+	 * @enduml
+	 */
 	switch (Copy_Channel){
 	case DMA_CH1 :
 		DMA_CH1_BASE->CCR  |= 0x00000001 ;
@@ -92,12 +198,66 @@ void DMA_ChannelEnable(Channel_DMA Copy_Channel){
 		DMA_CH7_BASE->CCR  |= 0x00000001 ;
 		break ;
 	default :
-		/* invalid parameter */
+		Det_ReportError(0x00, 0x00, 0x00);
 	}
 }
 
-/* DMA channel disable */
+/**
+ * \section Brief
+ * This function disables DMA channel.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Channel_DMA  \ Type enum  \ Range determine from Channel_DMA \ Resolution inapplicable \ unit bits
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_ChannelEnable -> CLR_BIT  : Clear bit Request
+ * CLR_BIT --> DMA_ChannelEnable : Clearing specified bit response
+ *
+ * @enduml
+ */
 void DMA_ChannelDisable(Channel_DMA Copy_Channel){
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *   switch (Which DMA channel is activited)
+	 *   case ( condition channel1 )
+	 *     #green:Turn off channel1;
+	 *   case ( condition channel2 )
+	 *     #green:Turn off channel2;
+	 *   case ( condition channel3 )
+	 *     #green:Turn off channel3;
+	 *   case ( condition channel4 )
+	 *     #green:Turn off channel4;
+	 *   case ( condition channel5 )
+	 *     #green:Turn off channel5;
+	 *   case ( condition channel6 )
+	 *     #green:Turn off channel6;
+	 *   case ( condition channel7 )
+	 *     #green:Turn off channel7;
+	 *   case ( default )
+	 *     #red:Det_ReportError(infinite loop);
+	 *     end
+	 * endswitch
+	 * end
+	 * @enduml
+	 */
 	switch (Copy_Channel){
 	case DMA_CH1 :
 		CLR_BIT(DMA_CH1_BASE->CCR ,0) ;
@@ -121,14 +281,72 @@ void DMA_ChannelDisable(Channel_DMA Copy_Channel){
 		CLR_BIT(DMA_CH7_BASE->CCR ,0) ;
 		break ;
 	default :
-		/* invalid parameter */
+		Det_ReportError(0x00, 0x00, 0x00);
 	}
 }
 
-/* DMA Start Transfer */
+/**
+ * \section Brief
+ * This function set address for each DMA channel that used in transformation data between them.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Copy_Channel      \ Type enum             \ Range determine from Channel_DMA \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_pBuffer      \ Type pointer to void  \ Range generic pointer            \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_pPeripheral  \ Type pointer to void  \ Range generic pointer            \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_NumOfElemnts \ Type uint16           \ Range 0:65636                    \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_Size         \ Type enum             \ Range determine from Size_DMA    \ Resolution inapplicable \ unit bits
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_SetAddress -> DMA_SetAddress_Help  : Configure perapheral address, Memory address, Element size, DMA channel Request
+ * DMA_SetAddress_Help --> DMA_SetAddress : Configuration response
+ *
+ * @enduml
+ */
 void DMA_SetAddress(Channel_DMA Copy_Channel, void *Copy_pBuffer,
 		void *Copy_pPeripheral, uint16 Copy_NumOfElemnts, Size_DMA Copy_Size) {
 
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *   switch (Which DMA channel is activited)
+	 *   case ( condition channel1 )
+	 *     #green:call DMA_SetAddress_Help for channel1;
+	 *   case ( condition channel2 )
+	 *     #green:call DMA_SetAddress_Help for channel2;
+	 *   case ( condition channel3 )
+	 *     #green:call DMA_SetAddress_Help for channel3;
+	 *   case ( condition channel4 )
+	 *     #green:call DMA_SetAddress_Help for channel4;
+	 *   case ( condition channel5 )
+	 *     #green:call DMA_SetAddress_Help for channel5;
+	 *   case ( condition channel6 )
+	 *     #green:call DMA_SetAddress_Help for channel6;
+	 *   case ( condition channel7 )
+	 *     #green:call DMA_SetAddress_Help for channel7;
+	 *   case ( default )
+	 *     #red:Det_ReportError(infinite loop);
+	 *     end
+	 * endswitch
+	 * end
+	 * @enduml
+	 */
 	switch (Copy_Channel){
 	case DMA_CH1 :
 		DMA_SetAddress_Help(DMA_CH1_BASE, Copy_pBuffer, Copy_pPeripheral,
@@ -159,12 +377,78 @@ void DMA_SetAddress(Channel_DMA Copy_Channel, void *Copy_pBuffer,
 				Copy_NumOfElemnts, Copy_Size);
 		break ;
 	default :
-		/* invalid parameter */
+		Det_ReportError(0x00, 0x00, 0x00);
 	}
 }
 
+/**
+ * \section Brief
+ * This function return flag state of DMA channel interrupt.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Private
+ *
+ * \param[in]     Copy_DMA_Base      \ Type pointer to structure  \ Range 0x40020000 : 0x4002008C \ Resolution 0x04         \ unit Address
+ * \param[in]     Copy_pBuffer       \ Type address               \ Range Unapplicable            \ Resolution Unapplicable \ unit Address
+ * \param[in]     Copy_pPeripheral   \ Type address               \ Range Unapplicable            \ Resolution Unapplicable \ unit Address
+ * \param[in]     Copy_NumOfElemnts  \ Type uint16                \ Range 0:65536                 \ Resolution 1            \ unit elements
+ * \param[in]     Copy_Size          \ Type uint8                \ Range 0:2                      \ Resolution 1            \ unit size of element
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_SetAddress_Help ->   :
+ *
+ * @enduml
+ */
 void DMA_SetAddress_Help(DMA_REG *DMA_Base, void *Copy_pBuffer,
 		void *Copy_pPeripheral, uint16 Copy_NumOfElemnts, uint8 Copy_Size){
+
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *   switch (Which DMA channel is activited)
+	 *   case ( condition channel1 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel1;
+	 *   case ( condition channel2 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel2;
+	 *   case ( condition channel3 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel3;
+	 *   case ( condition channel4 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel4;
+	 *   case ( condition channel5 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel5;
+	 *   case ( condition channel6 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel6;
+	 *   case ( condition channel7 )
+	 *     #green:Set peripheral address, memory address and element size
+	 *     that transimittied between them using channel7;
+	 *   case ( default )
+	 *     #red:Det_ReportError(infinite loop);
+	 *     end
+	 * endswitch
+	 * end
+	 * @enduml
+	 */
 	switch (Copy_Size){
 	case Size_8Bits :
 		DMA_Base->CMAR  = (uint8*) Copy_pBuffer ;
@@ -183,18 +467,95 @@ void DMA_SetAddress_Help(DMA_REG *DMA_Base, void *Copy_pBuffer,
 		SET_BIT(DMA_Base->CCR,11) ;
 		break ;
 	default :
-		/* Invalid Input */
+		Det_ReportError(0x00, 0x00, 0x00);
 	}
 	DMA_Base->CNDTR = Copy_NumOfElemnts ;
 }
 
+/**
+ * \section Brief
+ * This function clear flag of DMA channel interrupt.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Channel_DMA  \ Type enum  \ Range determine from Channel_DMA \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_FLAG    \ Type uint8 \ Range 1:7                        \ Resolution 1            \ unit bits
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_ClearFlag -> SET_BIT  : Setting bit Request to clear flag
+ * SET_BIT --> DMA_ClearFlag : Sething specified bit response
+ *
+ * @enduml
+ */
 void DMA_ClearFlag(Channel_DMA Copy_Channel ,uint8 Copy_FLAG) {
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *  :clear flag by setting the flag bit interrupt;
+	 * end
+	 * @enduml
+	 *
+	 */
 	SET_BIT(DMA_CH1_BASE->IFCR, (Copy_FLAG+ISR_REG_BASE*Copy_Channel) );
 }
 
 
-/* DMA Channel Status Check */
+
+/**
+ * \section Brief
+ * This function return flag state of DMA channel interrupt.
+ *
+ * \section Re-enterancy
+ * Re-Entrant
+ *
+ * \section Scoope
+ * Public
+ *
+ * \param[in]     Channel_DMA  \ Type enum  \ Range determine from Channel_DMA \ Resolution inapplicable \ unit bits
+ * \param[in]     Copy_FLAG    \ Type uint8 \ Range 1:7                        \ Resolution 1            \ unit bits
+ * \param[out]    None
+ * \param[in,out] None
+ * \return        None
+ *
+ * \section Sequence
+ *
+ * @startuml
+ *
+ * footer Page %page% of %lastpage%
+ *
+ * title Sequency diagram
+ *
+ * DMA_GetFlagStatus ->   :
+ *
+ * @enduml
+ */
 FlagStatus DMA_GetFlagStatus(Channel_DMA Copy_Channel ,Int_Type Copy_FLAG) {
+	/**
+	 * \section Activity
+	 *
+	 * @startuml
+	 *   start
+	 *  :Read flag of interrupt;
+	 * end
+	 * @enduml
+	 *
+	 */
 	uint8 Local_FlagState = GET_BIT(DMA_CH1_BASE->ISR,
 			(Copy_FLAG+ISR_REG_BASE*Copy_Channel) );
 	return Local_FlagState;
--- 01-MCAL/07-DMA/V1/DMA_Private.h
+++ 01-MCAL/07-DMA/V1/DMA_Private.h
@@ -6,26 +6,125 @@
 *@author     Shehab aldeen mohammed abdalah
 */
 
+
+/**
+* \file DMA_Private.h
+*
+* \brief Contains private defines and addresses of registers for the MPU (Memory Protection Unit) driver.
+*
+* \section Scope
+* Private
+*
+* \section Author
+* Shehab Abdalah
+*/
+
 #ifndef DMA_PRIVATE_H_
 #define DMA_PRIVATE_H_
 
+
+/**
+ * \section Brief
+ * Structure used in a pointer that helps access registers in the DMA peripheral.
+ *
+ * \section Details (Type Structure  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
 typedef struct {
+    /**
+     * \section Brief
+     * interrupt status register.
+     */
 	volatile uint32 ISR;
+    /**
+     * \section Brief
+     *  interrupt flag clear register.
+     */
 	volatile uint32 IFCR;
+    /**
+     * \section Brief
+     * channel x configuration register.
+     */
 	volatile uint32 CCR;
+    /**
+     * \section Brief
+     * channel x number of data register.
+     */
 	volatile uint32 CNDTR;
+    /**
+     * \section Brief
+     *  channel x peripheral address register .
+     */
 	volatile uint32 CPAR;
+    /**
+     * \section Brief
+     * channel x memory address register.
+     */
 	volatile uint32 CMAR;
 }DMA_REG;
 
-#define DMA_CH1_BASE           ((DMA_REG *)  (0x40020000))
-#define DMA_CH2_BASE           ((DMA_REG *)  (0x4002001C))
-#define DMA_CH3_BASE           ((DMA_REG *)  (0x40020030))
-#define DMA_CH4_BASE           ((DMA_REG *)  (0x40020044))
-#define DMA_CH5_BASE           ((DMA_REG *)  (0x40020058))
-#define DMA_CH6_BASE           ((DMA_REG *)  (0x4002006C))
-#define DMA_CH7_BASE           ((DMA_REG *)  (0x40020080))
 
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 1.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH1_BASE           ((DMA_REG *)  (0x40020000))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 2.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH2_BASE           ((DMA_REG *)  (0x40020014))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 3.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH3_BASE           ((DMA_REG *)  (0x40020028))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 4.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH4_BASE           ((DMA_REG *)  (0x4002003C))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 5.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH5_BASE           ((DMA_REG *)  (0x40020050))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 6.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH6_BASE           ((DMA_REG *)  (0x40020064))
+
+/**
+ * \section Brief
+ * Pointer to a structure used to access registers in the DMA peripheral channel 7.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit Address )
+ */
+#define DMA_CH7_BASE           ((DMA_REG *)  (0x40020078))
+
+/**
+ * \section Brief
+ * It's consider the number of bits that separate between flags in ISR register.
+ *
+ * \section Details (Type Macro  \ Range Unapplicable  \ Resolution Unapplicable \ unit bits )
+ */
 #define ISR_REG_BASE    4
 
 void DMA_SetAddress_Help(DMA_REG *Copy_DMA_Base, void *Copy_pBuffer,
